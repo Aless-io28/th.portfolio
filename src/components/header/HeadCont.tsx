8b325b56043416b577dc8fa3ff72309e5ffb20b8
Alessio <Alessio@egmail.com>
2024-10-29 14:48:09 -0500

    image head

--- src/components/header/HeadCont.tsx
+++ src/components/header/HeadCont.tsx
@@ -3,11 +3,11 @@ const HeadCont = () => {
     <header className="relative">
       <div className="w-full h-[70px] md:h-28"></div>
       <div className="absolute top-0 flex justify-between w-full overflow-hidden rounded-t-lg h-52 md:h-60 -z-10 md:top-4 bg-back-sec">
-        {/* <img
+        <img
           src="/hero.jpg"
           alt=""
           className="object-cover object-top w-full h-full"
-        /> */}
+        />
       </div>
     </header>
   );
